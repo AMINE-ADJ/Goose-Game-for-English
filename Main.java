4af589a8bb9bd565b8652588ef62697d6590d6a9
nednassim <jn_nedjar@esi.dz>
2022-04-03 12:22:50 +0100

    45%

--- Main.java
+++ Main.java
@@ -7,27 +7,28 @@ class Joueur {
    private String nom;
    private int score;
 
-   Joueur(int id, String nom, int score) {
+   Joueur(int id, String nom) {
       this.id = id;
       this.nom = nom;
-      this.score = score;
    }
 
    public void update_score(int  nouveau_score) {
       this.score = nouveau_score;
       System.out.println("");
-         
    }
       
-   @Override 
-   public String toString() {
-      return (nom + " [" + score + "]");
-   }
+   public int get_id() { return id;}
+   public String get_nom() { return nom;}
+
+   public void set_id(int id) { this.id = id;}
+   public void set_nom(String nom) { this.nom = nom;}
 
 }
 
+
 // La classe des des
 class De {
+
    public static int lancer_de () {
       return 1 + (int) (Math.random() % 6);
    }
@@ -63,10 +64,65 @@ class De {
 
 // La classe du plateau
 class Plateau {
-   int D1, D2;    // les deux des 
+
+/*
+        Le plateau :
+
+          -------  -------  -------  -------  -------  -------
+          |+ - +|  |+ - +|  |+ - +|  |+ - +|  |+ - +|  |+ - +|
+          |+   +|<-|+ R +|<-|+   +|<-|+ V +|<-|+   +|<-|+ J +| 
+          |+ - +|  |+ - +|  |+ - +|  |+ - +|  |+ - +|  |+ - +|
+          -------  -------  -------  -------  -------  -------
+             |                                          Debut         
+          -------  -------  -------  -------  -------  -------
+          |+ - +|  |+ - +|  |+ - +|  |+ - +|  |+ - +|  |+ - +|
+          |+ O +|  |+ R +|<-|+   +|<-|+   +|<-|+ V +|<-|+ R +|
+          |+ - +|  |+ - +|  |+ - +|  |+ - +|  |+ - +|  |+ - +| 
+          -------  -------  -------  -------  -------  -------
+             |        |                                   |
+          -------  -------  -------  -------  -------  -------
+          |+ - +|  |+ - +|  |+ - +|  |+ - +|  |+ - +|  |+ - +|
+   |      |+   +|  |+ R +|  |+ V +|<-|+   +|<-|+ O +|  |+   +|    /|\
+   |      |+ - +|  |+ - +|  |+ - +|  |+ - +|  |+ - +|  |+ - +|     |
+  \|/     -------  -------  -------  -------  -------  -------     |
+             |        |        |        |        |        |
+          -------  -------  -------  -------  -------  -------
+          |+ - +|  |+ - +|  |+ - +|  |+ - +|  |+ - +|  |+ - +|
+          |+   +|  |+ R +|  |+ B +|->|+ N +|  |+   +|  |+   +|
+          |+ - +|  |+ - +|  |+ - +|  |+ - +|  |+ - +|  |+ - +|
+          -------  -------  -------  -------  -------  -------
+             |        |                Fin       |        | 
+          -------  -------  -------  -------  -------  -------
+          |+ - +|  |+ - +|  |+ - +|  |+ - +|  |+ - +|  |+ - +|
+          |+ B +|  |+   +|->|+   +|->|+ B +|->|+ - +|  |+ - +|
+          |+ - +|  |+ - +|  |+ - +|  |+ - +|  |+ - +|  |+ - +|
+          -------  -------  -------  -------  -------  -------
+             |                                            |
+          -------  -------  -------  -------  -------  -------
+          |+ - +|  |+ - +|  |+ - +|  |+ - +|  |+ - +|  |+ - +|
+          |+   +|->|+ V +|->|+   +|->|+   +|->|+ B +|->|+ O +|
+          |+ - +|  |+ - +|  |+ - +|  |+ - +|  |+ - +|  |+ - +|
+          -------  -------  -------  -------  -------  -------
+
+          Les deux des :
+
+            D1              D2          
+          +++++++        +++++++
+          +     +        + O O +
+          +  O  +        +  O  +
+          +     +        + O O +   
+          +++++++        +++++++
+
+*/
+
+   // Le plateau contient deux des et 100 cases disposees en spirale
+   De D1, D2;    // les deux des 
    Case[] cases = new Case[100];   // le plateau a 100 cases
 
    public Plateau () {
+      D1 = new De(De.lancer_de());
+      D2 = new De(De.la);
+
       Set<Integer> set = new HashSet<Integer> ();    // l'ensemble des cases
       Random rand = new Random();
 
@@ -100,52 +156,78 @@ class Plateau {
          cases[(int) value.next()] = new Case_image();
       } 
    }
-   
-
 }
 
 // La classe d'une partie
 class Partie {
    private Joueur joueur;
    private Plateau plateau;
+   private int score;
+
+   public Partie (int id, String nom_joueur) {
+      joueur = new Joueur(id, nom_joueur);
+      plateau = new Plateau();
+   }
+
+   public int get_score() { return score;}
+   public void set_score(int score) { this.score = score;}
 
 }
 
 // La classe d'une case du plateau
-class Case {
-     
+interface Case {
+   public void mouvement();   // la methode qui englobe l'avancement et le recul
+   public void score();       // la methode qui englobe le gain et la perte
+
+}
+
+class Case_bonus implements Case {
+   private final String couleur = "VERTE";   
+   public void mouvement() {
       
+         
+   }
+   public void score() {
+   }
 }
 
-class Case_bonus extends Case {
-   private final String couleur = "VERTE";
-
-}
-
-class Case_malus extends Case {
+class Case_malus implements Case {
    private final String couleur = "ROUGE";
 
 }
 
-class Case_saut extends Case {
+class Case_saut implements Case {
    private final String couleur = "ORANGE";
 }
 
-class Case_definition extends Case {
+class Case_definition implements Case {
    private final String couleur = "BLEU";
 
 }
 
-class Case_image extends Case {
+class Case_image implements Case {
    private final String couleur = "ROSE";
 
 }
 
+
+// La classe du jeu 
 class Jeu {
-   Partie[] parties = new Partie[3];
+   public static int high_score = 0;
+   public Partie[] parties;
+
+   public Jeu (String[] joueurs) {
+      parties = new Partie[joueurs.size()];      // On lance 2 partie a la fois
+      for (int i = 0; i < joueurs.size(); i++) {
+         parties[0] = new Partie(i + 1, joueurs[i]);   
+      }
+   }
+
+   public void jouer() {
+   }
+
+
 }
-
-
 
 
 
